9e726510f305b94deea2d650e31a1bbef0554abc
Warren McDonald <warrenjmac@mail.com>
2019-10-29 20:11:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
   def initialize (name)
-    @name = name 
+    @name= name 
   end 
   def name
     @name 
   end 
-  def name = (new_name)
+  def name= (new_name)
    @new_name = new_name
   end
   def initialize (breed)
