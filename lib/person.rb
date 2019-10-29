7a5f2a98e43630d6eb931ed76492c5cf89583015
Warren McDonald <warrenjmac@mail.com>
2019-10-29 20:36:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,14 @@
 class Person
- def initialize (name)
-   @name = name
+ def name
+   @name 
  end
- def name 
-  @name
+ def name= (name)
+  @name = name
  end
- def name= (new_name)
-  @name = new_name
- end
- def initialize (job) 
-   @job= job
- end 
  def job
    @job
  end 
- def job= (new_job)
-   @job = new_job
+ def job= (job)
+   @job = job
  end
  end
